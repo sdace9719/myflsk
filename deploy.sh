29f61f704f9a34663a4d058744ca906037a38645
Cryptik <cryptik@www.ex2.com>
2020-08-26 09:10:21 +0530

    Removed sensitive credentials

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
-export DOCKER_HOST=tcp://18.237.124.77:2375
+export DOCKER_HOST=tcp://<HOST_IP>:<HOST_PORT>
 export DOCKER_TLS_VERIFY=
 
+
+#remove previous running container
 if [ "$(docker ps -a | grep flsktest)" ]
 then
     docker rm -f flsktest
 fi
-docker pull sw9719/circleciflasktest:latest
-docker run -d -p 5555:5555 --name flsktest sw9719/circleciflasktest:latest
+
+#deploy new container
+docker pull <Your Image>
+docker run -d -p 5555:5555 --name flsktest <Pulled Image>
 
